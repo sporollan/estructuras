c0410910b96e3356098356c296c5e1553ce78741
Santiago Porollan <santiago.porollan@gmail.com>
2023-04-12 12:41:58 -0300

    init lista

---
+++ lineales/dinamicas/Lista.java
@@ -0,0 +1,49 @@
+package lineales.dinamicas;
+
+
+public class Lista {
+    private Nodo cabecera;
+
+    public Lista(){
+
+    }
+
+    public boolean insertar(Object obj, int pos){
+        return false;
+    }
+
+    public boolean eliminar(int pos){
+        return false;
+    }
+
+    public Object recuperar(int pos){
+        return null;
+    }
+
+    public int localizar(Object obj){
+        return 0;
+    }
+
+    public int longitud(){
+        return 0;
+    }
+
+    public boolean esVacia(){
+        return false;
+    }
+
+    public void vaciar(){
+
+    }
+
+    public Lista clone(){
+        return null;
+    }
+
+    @Override
+    public String toString(){
+        String s = "";
+        return s;
+    }
+
+}
